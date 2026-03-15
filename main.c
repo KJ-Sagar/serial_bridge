a1cae6846eb3acfe92fdce3ac7e6bcab9b671e71
KJ-Sagar <kjagannathsagar@gmail.com>
2026-03-15 21:00:53 +0530

    March 15th edits

--- main.c
+++ main.c
@@ -2,36 +2,59 @@
 #include "pico/stdlib.h"
 #include "pico_espat_udp_utils.h"
 
-char WIFI_SSID[] = "D108-6G";
-char WIFI_PASSWORD[] = "9533112237";
+#define WIFI_SSID "D108-4G"
+#define WIFI_PASSWORD "9533112237"
+
+#define TCP_SERVER_PORT 20030
 
 int main() {
     stdio_init_all();
     
-    
     sleep_ms(10000);
-    printf("Testing ESPAT\n");
-    printf("%d\n", espat_init());
 
-    printf("Testing WifiConn\n");
-    printf("%d\n", connect_to_wifi(WIFI_SSID, WIFI_PASSWORD));
+    if(espat_init()) {
+        printf("WiFi module initialized\n");
+    }
+    else {
+        printf("Failed to initalize WiFi Module\n");
+    }
 
+    if(connect_wifi(WIFI_SSID, WIFI_PASSWORD)) {
+        printf("Connected to '%s'\n", WIFI_SSID);
+    }
+    else {
+        printf("Unable to connect to WiFi\n");
+    }
 
-    
+    if(start_tcp_server(20030)) {
+        printf("Started TCP server at port '%d'\n", TCP_SERVER_PORT);
+    }
+    else {
+        printf("Unable to start TCP server\n");
+    }
+
+    printf("Entering bridge mode\n");
     while (true) {
-        // PC → ESP8266
+        // USB to UART bridging
         int ch = getchar_timeout_us(0);
         if (ch != PICO_ERROR_TIMEOUT) {
             uart_putc(UART_ID, (char)ch);
         }
-
-        // ESP8266 → PC
-        if (uart_is_readable(UART_ID)) {
-            char c = uart_getc(UART_ID);
-            putchar(c);
-        }
     }
 
+    if(stop_tcp_server()) {
+        printf("Stopped the TCP server\n");
+    }
+    else {
+        printf("Unable to stop the TCP server\n");
+    }
+
+    if(disconnect_wifi(WIFI_SSID, WIFI_PASSWORD)) {
+        printf("Disconnected from '%s'\n", WIFI_SSID);
+    }
+    else {
+        printf("Unable to disconnect from WiFi\n");
+    }
 }
 
 
